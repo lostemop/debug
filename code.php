51be4ede71ec1bc1ac22afeec10f28d95664cc3e
kevin <284299280@qq.com>
2017-12-28 23:27:13 +0800

    fix

---
+++ code.php
@@ -0,0 +1,19 @@
+<?php
+//ini_set('xdebug.coverage_enable',1);
+xdebug_start_code_coverage();
+
+function a($a) {
+    echo $a * 2.5;
+}
+
+function b($count) {
+    for ($i = 0; $i < $count; $i++) {
+        a($i + 0.17);
+    }
+}
+
+b(6);
+//b(10);
+
+var_dump(xdebug_get_code_coverage());
+?>
--- dump.php
+++ dump.php
@@ -26,7 +26,7 @@ $data = array(
             'two.one.zero' => 210,
             'two.one.one' => array(
                 'two.one.one.zero' => 3.141592564,
-                'two.one.one.one'  => ['hello'],
+                'two.one.one.one'  => array('hello'),
             ),
         ),
     ),
